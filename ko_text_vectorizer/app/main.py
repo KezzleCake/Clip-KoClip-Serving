e701129b594626db13fd4340dfea49afa1cb479e
HanHyunsoo <gustn4563@gmail.com>
2023-09-03 02:33:22 +0900

    feat: add text to vector Lambdas

---
+++ ko_text_vectorizer/app/main.py
@@ -0,0 +1,37 @@
+import torch
+import json
+from transformers import AutoModel, AutoProcessor
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+repo = "./clip-vit-base-patch32-ko"
+model = AutoModel.from_pretrained(repo).to(DEVICE)
+processor = AutoProcessor.from_pretrained(repo)
+
+def get_vector(keyword):
+    keyword = processor(text=keyword, return_tensors="pt")
+
+    with torch.inference_mode():
+        vectors = model.get_text_features(**keyword)
+    return vectors.squeeze(0)
+
+def lambda_handler(event, context):
+    try:
+        body = json.loads(event['body'])
+        keyword = body['keyword']
+
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json; charset=utf-8"
+            },
+            "body": json.dumps({
+                "keyword": keyword,
+                "vector": get_vector(keyword).tolist()
+            }, ensure_ascii=False)
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "statusCode": 400,
+        }
